6cc8f76b69251f235bbcdce19cdb99e22a9f9d5a
xiaoen <2768753269@qq.com>
2026-01-30 21:23:00 +0800

    修复代码格式问题，统一gofmt格式化

--- app/activity/model/types.go
+++ app/activity/model/types.go
@@ -48,13 +48,10 @@ const (
 
 // BaseModel 基础模型（所有表共用）
 type BaseModel struct {
-	ID uint64 `gorm:"primaryKey;autoIncrement"    
-  json:"id"`
-	CreatedAt time.Time `gorm:"autoCreateTime"              
-  json:"created_at"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime"              
-  json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"` // 软删除
+	ID        uint64         `gorm:"primaryKey;autoIncrement" json:"id"`
+	CreatedAt time.Time      `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
 
 // 分页参数
