51ceb75f01934e9f4be345f2910eb4d6e7691fc8
xiaoen <2768753269@qq.com>
2026-01-31 15:39:05 +0800

    生成活动服务 RPC 框架代码

---
+++ app/activity/rpc/internal/logic/list_categories_logic.go
@@ -0,0 +1,31 @@
+package logic
+
+import (
+	"context"
+
+	"activity-platform/app/activity/rpc/activity"
+	"activity-platform/app/activity/rpc/internal/svc"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type ListCategoriesLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewListCategoriesLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ListCategoriesLogic {
+	return &ListCategoriesLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+// ==================== 分类标签接口 ====================
+func (l *ListCategoriesLogic) ListCategories(in *activity.ListCategoriesReq) (*activity.ListCategoriesResp, error) {
+	// todo: add your logic here and delete this line
+
+	return &activity.ListCategoriesResp{}, nil
+}
---
+++ app/activity/rpc/internal/logic/update_activity_logic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"activity-platform/app/activity/rpc/activity"
+	"activity-platform/app/activity/rpc/internal/svc"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type UpdateActivityLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewUpdateActivityLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UpdateActivityLogic {
+	return &UpdateActivityLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *UpdateActivityLogic) UpdateActivity(in *activity.UpdateActivityReq) (*activity.UpdateActivityResp, error) {
+	// todo: add your logic here and delete this line
+
+	return &activity.UpdateActivityResp{}, nil
+}
