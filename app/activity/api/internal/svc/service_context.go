98f16bd375baedd14df858b199b9760733828b88
xiaoen <2768753269@qq.com>
2026-02-02 16:32:30 +0800

    配置 API 层 ActivityRpc 客户端连接
    - Config 添加 ActivityRpc 配置结构
    - ServiceContext 初始化 ActivityRpc 客户端

--- app/activity/api/internal/svc/service_context.go
+++ app/activity/api/internal/svc/service_context.go
@@ -6,17 +6,29 @@ package svc
 import (
 	"activity-platform/app/activity/api/internal/config"
 	"activity-platform/app/activity/api/internal/middleware"
+	"activity-platform/app/activity/rpc/activityservice"
+
 	"github.com/zeromicro/go-zero/rest"
+	"github.com/zeromicro/go-zero/zrpc"
 )
 
 type ServiceContext struct {
-	Config    config.Config
+	Config config.Config
+
+	// 中间件
 	AdminAuth rest.Middleware
+
+	// RPC 客户端
+	ActivityRpc activityservice.ActivityService
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	// 初始化 Activity RPC 客户端
+	activityRpcClient := zrpc.MustNewClient(c.ActivityRpc)
+
 	return &ServiceContext{
-		Config:    c,
-		AdminAuth: middleware.NewAdminAuthMiddleware().Handle,
+		Config:      c,
+		AdminAuth:   middleware.NewAdminAuthMiddleware().Handle,
+		ActivityRpc: activityservice.NewActivityService(activityRpcClient),
 	}
 }
