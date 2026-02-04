d8fec58626a69bdf9dd609963aee2bf2d408b560
ycl <1503464068@qq.com>
2026-02-04 11:38:36 +0800

    修改了proto

---
+++ app/user/rpc/internal/logic/userbasicservice/refresh_token_logic.go
@@ -0,0 +1,31 @@
+package userbasicservicelogic
+
+import (
+	"context"
+
+	"activity-platform/app/user/rpc/internal/svc"
+	"activity-platform/app/user/rpc/pb/pb"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type RefreshTokenLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewRefreshTokenLogic(ctx context.Context, svcCtx *svc.ServiceContext) *RefreshTokenLogic {
+	return &RefreshTokenLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+// 刷新短token
+func (l *RefreshTokenLogic) RefreshToken(in *pb.RefreshReq) (*pb.RefreshResponse, error) {
+	// todo: add your logic here and delete this line
+
+	return &pb.RefreshResponse{}, nil
+}
