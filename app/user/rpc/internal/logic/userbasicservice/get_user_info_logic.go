abd62a726950d115bfb770cf85a17187e1269d4f
ycl <1503464068@qq.com>
2026-02-04 14:28:17 +0800

    把获取用户信息单独脱离出来

---
+++ app/user/rpc/internal/logic/userbasicservice/get_user_info_logic.go
@@ -0,0 +1,31 @@
+package userbasicservicelogic
+
+import (
+	"context"
+
+	"activity-platform/app/user/rpc/internal/svc"
+	"activity-platform/app/user/rpc/pb/pb"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type GetUserInfoLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewGetUserInfoLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetUserInfoLogic {
+	return &GetUserInfoLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+// 获取用户的信息
+func (l *GetUserInfoLogic) GetUserInfo(in *pb.GetUserInfoReq) (*pb.GetUserInfoResponse, error) {
+	// todo: add your logic here and delete this line
+
+	return &pb.GetUserInfoResponse{}, nil
+}
